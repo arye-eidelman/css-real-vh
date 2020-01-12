64469930deac3bef73a6d9e58b9ab0fe265b27a8
Arye Eidelman <aryedov.e@gmail.com>
2020-01-12 01:12:49 -0500

    Small style fixes and code prettification

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-document.getElementById("menu-toggle").addEventListener("click", function(e) {
+document.getElementById("menu-toggle").addEventListener("click", function (e) {
   document.getElementsByTagName("nav")[0].classList.toggle("--open")
 })
 
-Array.from(document.getElementsByClassName("navigation-slider-item")).forEach(function(item) {
+Array.from(document.getElementsByClassName("navigation-slider-item")).forEach(function (item) {
 
-  item.addEventListener("click", function(event) {
-    Array.from(document.getElementsByClassName("navigation-slider-item")).forEach(function(item) {
+  item.addEventListener("click", function (event) {
+    Array.from(document.getElementsByClassName("navigation-slider-item")).forEach(function (item) {
       item.classList.remove("--selected")
       item.classList.remove("btn-primary")
       item.classList.add("btn-secondary")
