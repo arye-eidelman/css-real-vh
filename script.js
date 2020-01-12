7994867c63250182b668a94da1dc69bbdb0dff43
Arye Eidelman <aryedov.e@gmail.com>
2020-01-12 01:00:31 -0500

    Created demo of vh units not not working correctly

---
+++ script.js
@@ -0,0 +1,18 @@
+document.getElementById("menu-toggle").addEventListener("click", function(e) {
+  document.getElementsByTagName("nav")[0].classList.toggle("--open")
+})
+
+Array.from(document.getElementsByClassName("navigation-slider-item")).forEach(function(item) {
+
+  item.addEventListener("click", function(event) {
+    Array.from(document.getElementsByClassName("navigation-slider-item")).forEach(function(item) {
+      item.classList.remove("--selected")
+      item.classList.remove("btn-primary")
+      item.classList.add("btn-secondary")
+    })
+
+    event.target.classList.add("--selected")
+    event.target.classList.add("btn-primary")
+    event.target.classList.remove("btn-secondary")
+  })
+})
